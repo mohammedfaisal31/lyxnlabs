f84cc644d2efbfcfda18dba90a1ded4e589b1a09
Mohammed Soharab S <soharab@quinta.co.in>
2023-05-21 00:36:15 +0530

    Effects added and banner done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import {fadeIn} from "../variants";
+import Image from "../assets/avatar.svg";
+import {AiFillGithub,AiFillLinkedin,AiFillFacebook,AiFillInstagram} from "react-icons/ai";
+import { motion } from "framer-motion";
+ 
 const Banner = () => {
-  return <div className='section' id='home'>
+  return <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
     <div className='container mx-auto'>
-        <div>
-          <div>
-            <div className='mb-6 text-[60px] lg:text-[90px] font-secondary uppercase leading-[1]' >
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12' >
+          <div className='flex-1 text-center font-secondary '>
+            <motion.div 
+              variants={fadeIn("up",0.6)}
+              initial={"hidden"}
+              whileInView={"show"}
+              viewport={{once:false, amount:0.7}}
+              className='mb-6 text-[60px] lg:text-[90px] font-secondary uppercase leading-[1]' >
               <span className='mr-4'>You think | </span>
               <TypeAnimation
                 sequence={[
@@ -22,14 +31,46 @@ const Banner = () => {
                 wrapper='span'
                 repeat={Infinity}
               />
-            </div>
-            <p>
-            Quantum computing is a fascinating field that uses the principles of quantum mechanics to perform computations in a completely different way than traditional computers. In traditional computers, we use bits to store and process information, which can be either a 0 or a 1. But in quantum computers, we use quantum bits, or qubits, which can represent 0, 1, or both at the same time.
-            </p>
+            </motion.div>
+            <motion.p 
+              variants={fadeIn("up",0.8)}
+              initial={"hidden"}
+              whileInView={"show"}
+              viewport={{once:false, amount:0.7}}
+            >
+                Quantum computing is a fascinating field that uses the principles of quantum mechanics to perform computations in a completely different way than traditional computers. In traditional computers, we use bits to store and process information, which can be either a 0 or a 1. But in quantum computers, we use quantum bits, or qubits, which can represent 0, 1, or both at the same time.
+                Quantum computing is a fascinating field that uses the principles of quantum mechanics to perform computations in a completely different way than traditional computers. In traditional computers, we use bits to store and process information, which can be either a 0 or a 1. But in quantum computers, we use quantum bits, or qubits, which can represent 0, 1, or both at the same time.
+                  
+            </motion.p>
+            <motion.div 
+              variants={fadeIn("up",0.8)}
+              initial={"hidden"}
+              whileInView={"show"}
+              viewport={{once:false, amount:0.7}}
+              className='items-center mb-10 mt-10 mx-auto lg:mx-0 '>
+              <button className='btn btn-lg'>Explore with us</button>
+            </motion.div>
+
+            {/* <div className='flex items-center text-[25px] gap-x-6 '>
+              <a href="#">
+                <AiFillGithub/>
+              </a>
+              <a href="#">
+                <AiFillLinkedin/>
+              </a>
+              <a href="#">
+                <AiFillFacebook/>
+              </a>
+              <a href="#">
+                <AiFillInstagram/>
+              </a>
+              
+            </div> */}
           </div>
+          {/* <img src={Image} width={500} height={500} alt={"logo"}/> */}
         </div>
     </div>
-  </div>;
+  </section>;
 };
 
 export default Banner;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,30 @@
 import React from 'react';
-
+import { motion } from "framer-motion";
+import { fadeIn } from '../variants';
 const Header = () => {
   return(
     <header className='py-8'>
       <div className='container mx-auto'>
         <div className='flex justify-between items-center'>
           <a href="/">
-            <img alt="logo"/>
+            <motion.h1
+              variants={fadeIn("up",0.3)}
+              initial={"hidden"}
+              whileInView={"show"}
+              viewport={{once:false, amount:0.7}}
+              className='text-[35px] semi-bold'
+            >
+                LYXN LABS
+            </motion.h1>
           </a>
-          <button className='btn btn-sm '>Get into business</button>
+          <motion.button 
+            variants={fadeIn("up",0.3)}
+            initial={"hidden"}
+            whileInView={"show"}
+            viewport={{once:false, amount:0.7}}
+            className='btn btn-sm '>
+            Contact us
+          </motion.button>
         </div>
       </div>
     </header>
